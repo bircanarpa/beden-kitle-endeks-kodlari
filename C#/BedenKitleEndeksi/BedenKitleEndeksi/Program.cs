dfbc9c01b629daa20757b8bc53557eaaf20cfe1a
Bircan Arpa <119975907+bircanarpa@users.noreply.github.com>
2023-06-22 16:30:09 +0300

    beden-kitle-endeks-olcumu
    C# if else bke kodları

---
+++ C#/BedenKitleEndeksi/BedenKitleEndeksi/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedenKitleEndeksi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kilonuzu Giriniz");
+            int kilo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Boyunuzu Giriniz");
+            double boy = Convert.ToDouble(Console.ReadLine());
+
+            double bke = kilo / (boy * boy);
+
+            if (bke < 18)
+            {
+                Console.WriteLine("Zayıfsınız...");
+                Console.ReadLine();
+            }
+            else if (bke>18 && bke<25)
+
+            {
+                Console.WriteLine("Normalsiniz");
+            }
+            else if(bke>25)
+            {
+                Console.WriteLine("Obezsiniz");
+            }
+            
+
+        }
+        
+
+       
+        
+           
+
+    }
+}
